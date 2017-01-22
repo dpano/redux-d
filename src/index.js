e935f1e09ee7aafbf460d0296f0129cb1861bc0a
dpano <d4pano@gmail.com>
2017-01-23 00:31:42 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDAoexzbf38OOjJiYQECMlt1IbUV1B4KBE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+);
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
