d30e96acf04dcb73e7c21692c23d877023e52836
dpano <d4pano@gmail.com>
2017-01-24 23:50:49 +0100

    section 26

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDAoexzbf38OOjJiYQECMlt1IbUV1B4KBE';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-);
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
